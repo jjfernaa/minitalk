3f8093db57e9dd5c5f16da734aee21bf9c026fba
Juan Fernández <juanjose.fernanfez88@gmail.com>
2025-04-26 03:06:02 +0200

    test-f

--- client.c
+++ client.c
@@ -6,7 +6,87 @@
 /*   By: juan-jof <juan-jof@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 02:20:54 by juan-jof          #+#    #+#             */
-/*   Updated: 2025/03/29 02:20:55 by juan-jof         ###   ########.fr       */
+/*   Updated: 2025/04/26 03:03:08 by juan-jof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
+
+int	ft_atoi(const char *str)
+{
+	int	result;
+	int	sign;
+
+	result = 0;
+	sign = 1;
+	while(*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-')
+		sign = -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	return (result * sign);
+}
+
+void	send_char(int pid, char c)
+{
+	int	bit;
+	int	i;
+
+	i = 7;
+	while (i >= 0)
+	{
+		bit = (c >> i) & 1;
+		if (bit == 0)
+		{
+			if (kill(pid, SIGUSR1) == -1)
+			{
+				ft_putstr_fd("Error: No se pudo enviar la señal SIGUSR1\n", 2);
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+			{
+				ft_putstr_fd("Error : No se pudo enviar la señal SIGUSR2\n", 2);
+				exit(EXIT_FAILURE);
+			}
+		}
+		usleep(100);
+		i--;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int pid;
+	int i;
+
+	if (argc != 3)
+	{
+		ft_putstr_fd("Uso: ./client [PID del servido] [mensaje]\n", 2);
+		return (1);
+	}
+	
+	pid = ft_atoi(argv[1]);
+	if (pid <= 0)
+	{
+		ft_putstr_fd("Error_ PID invalido\n", 2);
+		return (1);
+	}
+	i = 0;
+	while (argv[2][i])
+	{
+		send_char(pid, argv[2][i]);
+		i++;
+	}
+	
+	send_char(pid, '\n');
+	return (0);
+}
---
+++ server.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   srver.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juan-jof <juan-jof@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/29 02:21:14 by juan-jof          #+#    #+#             */
+/*   Updated: 2025/04/26 02:44:52 by juan-jof         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+static char	g_char = 0;
+static int	g_bit_count = 0;
+
+void	handle_signal(int signum)
+{
+	if (signum == SIGUSR1)
+		g_char = g_char << 1;
+	else if (signum == SIGUSR2)
+		g_char = (g_char << 1) | 1;
+		
+	g_bit_count++;
+	
+	if (g_bit_count == 8)
+	{
+		write(1, &g_char, 1);
+		g_char = 0;
+		g_bit_count = 0;
+	}
+}
+
+int	main(void)
+{
+	struct sigaction	sa;
+	
+	sa.sa_handler = &handle_signal;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+	
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
+	{
+		ft_putstr_fd("Error: No se pudo configurar el manejador de señales\n", 2);
+		return (1);
+	}
+	
+	ft_putstr_fd("Servidor iniciado con PID:", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putstr_fd("\n", 1);
+	
+	while (1)
+		pause();
+	return (0);
+}
